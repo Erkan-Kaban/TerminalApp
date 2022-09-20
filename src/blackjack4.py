0db0215aaf2c282b05bb1fded1c3527293686c26
Erkan <12980@coderacademy.edu.au>
2022-09-20 16:18:46 +1000

    backed up files

--- src/blackjack4.py
+++ src/blackjack4.py
@@ -4,10 +4,9 @@ from deck import Deck
 
 class Blackjack(Money):
     def blackjack_table(self) -> None:
-        print("Welcome to BlackJack! \nYou as the player will start with 500 credits \n ")
+        print('Welcome to BlackJack! \nYou as the player will start with 500 credits \n ')
         # Cards are shuffled.
-        cards.shuffle()
-        print("Cards are shuffling")     
+         
 
     # Player starts at 500 credits.
     def player(self, credits = 500) -> None:
@@ -15,19 +14,21 @@ class Blackjack(Money):
             player_money = Money() # Instance of a class here from money.py
             player_hand_total = 0
             # player can make a bet up to and including 500.
+            cards.shuffle()
+            print('Cards are shuffling')    
             bet_amount = player_money.bet()
             # Draw_cards called under deck.py to deal a card to player
-            player_hand = cards.draw_cards("Deal")
+            player_hand = cards.draw_cards('Deal')
             # Contain the players card value in a variable player_hand_value
             player_hand_value = cards.get_values(player_hand)
-            print("Your hand is: \n")
+            print('Your hand is: \n')
             # Prints players hand
-            print(player_hand)
+            print(str(player_hand) + "\n")
 
             
             player_hand_total += self.get_values(player_hand_value)
             # Prints the total in a string
-            print('Total of: ' + str(player_hand_total))
+            print('Total of: ' + str(player_hand_total) + '\n')
         
             while(player_hand_total < 21):
                 player_hand_total = 0
@@ -41,14 +42,14 @@ class Blackjack(Money):
                     # for key, value in player_hand_value.items():
                         # player_hand_total += player_hand_value[key]
                     player_hand_total += self.get_values(player_hand_value)
-                    print('Total of: ' + str(player_hand_total))
+                    print('Total of: ' + str(player_hand_total) + '\n')
                     if player_hand_total > 21:
-                        print("Bust! You went over 21! you lose")
+                        print('Bust! You went over 21! you lose' + '\n')
 
             if player_hand_total == 21:
                 player_money.blackjack(bet_amount)
                 credits += bet_amount * 2.5
-                print("Your total credits are now: " + str(credits))   
+                print('Your total credits are now: ' + str(credits) + '\n')   
 
     # This loop function gets the players hand (two cards and adds up the sum value)              
     def get_values(self, values):
