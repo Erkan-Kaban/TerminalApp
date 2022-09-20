1dcaa5eef6fc4c8915b277fd26eb895bb33849dc
Erkan <12980@coderacademy.edu.au>
2022-09-20 22:05:58 +1000

    last for the evening

--- src/player_backup2.py
+++ src/player_backup2.py
@@ -1,22 +1,15 @@
 # from CardGame.Blackjack.money import Money
-from deck import Deck
-from black_jack_test import Player
+# import deck
+import black_jack_test
 
 class Blackjack():
-    def blackjack_table(self, player) -> None:
+    def blackjack_table(self, person) -> None:
         print('Welcome to BlackJack! \nYou as the player will start with $500 credits \n ')
         # Cards are shuffled.
          
     
-
-# Creating an instance of Deck to get handed a new deck.  
-# cards = Deck()
-# table = Blackjack()
-# table.blackjack_table(Player)
-# table.player()
-    
 table = Blackjack()
-table.blackjack_table(Player)
+table.blackjack_table(black_jack_test.Player)
 
 
     
